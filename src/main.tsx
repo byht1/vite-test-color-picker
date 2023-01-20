a5168c31e4d875617ff0adb810f758b65bc4051d
Vitalik Dykhal <vitalikbyht1@gmail.com>
2023-01-20 09:50:31 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="vite-test-color-picker">
+    <BrowserRouter basename="/vite-test-color-picker">
       <App />
     </BrowserRouter>
   </React.StrictMode>
