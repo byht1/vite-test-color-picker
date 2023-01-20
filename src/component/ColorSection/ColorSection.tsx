164f74f0100472d3e7c8d8cbe747529c2bda1e77
Vitalii Dykhal <99541944+byht1@users.noreply.github.com>
2023-01-20 17:07:23 +0200

    eslint test

--- src/component/ColorSection/ColorSection.tsx
+++ src/component/ColorSection/ColorSection.tsx
@@ -13,7 +13,7 @@ type Props = {
 
 export const ColorSection: FC<Props> = ({ defaultValue, index, set }) => {
   const isLock = defaultValue.lock
-  const [value, setValue] = useState(defaultValue)
+  const [value] = useState(defaultValue)
   const { hex, rgb } = value
   const rgbText = `rgb(${rgb.join(', ')})`
 
